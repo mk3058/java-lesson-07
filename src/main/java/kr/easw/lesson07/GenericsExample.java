a1eb0adf034b65aa7d1427b5eb07ea47cdf1d2e8
mk3058 <kmk3058@icloud.com>
2024-05-09 14:38:22 +0900

    GenericsExample 솔루션 추가

--- src/main/java/kr/easw/lesson07/GenericsExample.java
+++ src/main/java/kr/easw/lesson07/GenericsExample.java
@@ -3,14 +3,13 @@ package kr.easw.lesson07;
 
 /**
  * 해당 클래스는 익명 클래스 및 제너릭스에 대해 조금 더 친숙해지기 위해 구성된 문제입니다.
- *
- * 주석을 읽어 코드를 이해하고, 코드가 정상적으로 동작하도록 수정해보세요.
- * 해당 코드는 의도적으로 오류가 발생하도록 구성되었습니다.
+ * <p>
+ * 주석을 읽어 코드를 이해하고, 코드가 정상적으로 동작하도록 수정해보세요. 해당 코드는 의도적으로 오류가 발생하도록 구성되었습니다.
  */
 public class GenericsExample {
 
     public static void main(String[] args) {
-        Recipe<Ingredient, Cake> recipe = new Recipe<>(new RottenEgg()) {
+        Recipe<Ingredient, Cake> recipe = new Recipe<>(new Egg()) {
             @Override
             public Cake cook() {
                 // 제너릭스를 통해 클래스를 확정했음으로, T 제너릭스가 Ingredient를 상속받는것이 보장됩니다.
@@ -28,6 +27,7 @@ public class GenericsExample {
     // 위의 main 메서드를 보면, Recipe 옆의 꺾쇠 안에 2개의 클래스 이름이 적혀있습니다. 이것이 제너릭스의 확정입니다.
     // 이를 통해, Recipe 클래스는 Ingredient 클래스를 재료로, Cake 클래스를 반환값으로 사용하게 됩니다.
     abstract static class Recipe<T, R> {
+
         // 제너릭스를 사용하여 재료를 저장할 수 있게끔 합니다.
         private final T ingredient;
 
@@ -47,10 +47,12 @@ public class GenericsExample {
     // 제너릭스 자체로는 런타임상에서 타입을 알 수 없지만, 컴파일 타임에 타입을 알 수 있게끔 해줍니다.
     // 이로써, 파라미터 및 반환 타입에 동적으로 타입을 강제할 수 있기에 코드의 안정성을 높일 수 있습니다.
     interface Ingredient {
+
         void mix();
     }
 
     static class Flour implements Ingredient {
+
         @Override
         public void mix() {
             System.out.println("Mixing flour...");
@@ -58,6 +60,7 @@ public class GenericsExample {
     }
 
     static class RottenEgg implements Ingredient {
+
         @Override
         public void mix() {
             throw new RuntimeException("Rotten egg detected!");
@@ -65,6 +68,7 @@ public class GenericsExample {
     }
 
     static class Egg implements Ingredient {
+
         @Override
         public void mix() {
             System.out.println("Mixing egg...");
@@ -72,11 +76,11 @@ public class GenericsExample {
     }
 
     static class Cake {
+
         public void bake() {
             System.out.println("Done!");
         }
     }
-
 
 
 }
